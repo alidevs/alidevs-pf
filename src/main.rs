e92b2083698f210a8f63173f7d6a26388ec32cbb
alidevs <alialatyah2@gmail.com>
2024-08-08 20:03:16 +0300

    fixed mobile view on homepage

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ fn App() -> Html {
         <div class={classes!("bg-black", "flex", "flex-col", "items-center", "h-screen", "font-manrope")}>
             <div class={classes!("flex-grow", "flex", "flex-col", "items-center", "justify-center")}>
                 <p class={classes!(
-                    "my-2", "font-extrabold", "text-transparent", "text-6xl",
+                    "font-extrabold", "text-transparent", "text-6xl", "lg:text-7xl",
                     "font-bold", "bg-gradient-to-r", "from-grad-1",
                     "via-grad-3", "to-grad-4", "bg-clip-text"
                 )}>
                     {"Ali Alateyah"}
                 </p>
 
-                <p class={classes!("text-lg", "text-vista", "font-light")}>
+                <p class={classes!("text-lg", "text-vista", "font-extralight", "lg:text-xl")}>
                     {"Backend Developer"}
                 </p>
             </div>
 
             <div class={classes!(
-                "flex", "flex-row", "gap-x-4", "mb-4", "text-sm",
-                "text-grey-cloud", "font-light"
+                "flex", "flex-row", "gap-x-4", "mb-4", "text-sm", "lg:text-lg",
+                "text-grey-cloud", "font-medium"
             )}>
                 <a href="https://github.com/alidevs">{"GitHub"}</a>
             </div>
